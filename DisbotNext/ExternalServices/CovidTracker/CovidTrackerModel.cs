1e0d1d04986c8b1c4dbaa9dc00f8a31b5faef1f9
Deszolate_C <deszolate@icloud.com>
2021-04-09 21:36:01 +0700

    Add covid tracker to command function. (#9)

---
+++ DisbotNext/ExternalServices/CovidTracker/CovidTrackerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisbotNext.ExternalServices.CovidTracker
+{
+    public class CovidTrackerModel
+    {
+        public string Country { get; set; }
+        public int Cases { get; set; }
+        public int TodayCases { get; set; }
+        public int Deaths { get; set; }
+        public int TodayDeaths { get; set; }
+        public int Recovered { get; set; }
+        public int Active { get; set; }
+        public int Critical { get; set; }
+        public int CasesPerOneMillion { get; set; }
+    }
+}
