3e67f11db2c9e4981dca4f27daf5d2c2abc77315
Deszolate_C <deszolate@icloud.com>
2021-06-12 12:04:28 +0700

    A little fix to web and pipeline adjustment. (#40)
    * Fix mistakes from testing in DisbotNext.Api.
    
    * A little fix to web and pipeline adjustment.
    
    * Set default directory for node js CI.
    
    * Remove npm test from node js CI (there is no test project yet!).

---
+++ DisbotNext.Web/src/components/ConfigRow.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="content">
+    <div id="left">
+      <h5 class="label">{{ label }}:</h5>
+    </div>
+    <div id="right">
+      <div class="control">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    label: String,
+  },
+});
+</script>
+
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+#content {
+  padding: 0.5rem;
+  margin: 0;
+  width: 100%;
+}
+
+#left {
+  padding: 0.5rem;
+  display: inline-block;
+  width: 30%;
+}
+
+#right {
+  padding: 0.5rem;
+  display: inline-block;
+  width: 70%;
+}
+
+.label {
+  text-align: start;
+  /* background: lightcoral; */
+}
+
+.control {
+  display: flex;
+  margin-left: 10%;
+  /* background: lightgreen; */
+}
+</style>
